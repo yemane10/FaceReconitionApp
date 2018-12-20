5302ae7e7b8b651b70d97b40a3884e340fd05995
yemane10 <30352772+yemane10@users.noreply.github.com>
2018-12-20 13:21:52 -0600

    Update FaceRecognition.js
    fixing a typo error on FaceRecognition.js code.

--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -4,7 +4,7 @@ import './FaceRecognition.css';
 const FaceRecognition = ({imageUrl, box}) => {
 	return (
 		<div className='center ma'>
-			<div classname='absolute mt2'>
+			<div className='absolute mt2'>
 				<img id='inputImage' alt='' src={imageUrl} width='500px' height='auto'/>
 				<div 
 					className='bounding-box' 
